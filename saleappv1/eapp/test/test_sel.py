40a5caae6b2f0af114a7179e25b8b1301d18aa6f
minhhaqn05 <minhha29062005@gmail.com>
2026-05-15 15:52:48 +0700

    update

--- saleappv1/eapp/test/test_sel.py
+++ saleappv1/eapp/test/test_sel.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 from eapp.test.pages.CartPage import CartPage
 from eapp.test.pages.HomePage import HomePage
 from eapp.test.pages.LoginPage import LoginPage
+from eapp.test.pages.RegisterPage import RegisterPage
 from eapp.test.test_base import driver, test_app
 import time
 import pytest
@@ -100,3 +101,21 @@ def test_update_cart(driver):
 
     e = driver.find_element(By.CLASS_NAME, 'cart-counter')
     assert int(e.text) == 4
+
+def test_register(driver):
+    register = RegisterPage(driver=driver)
+    register.open_page()
+    register.register(name='demo', username='demodemo', password='Demo@123', confirm='Demo@123', avatar=r'D:\TestingSaleApp\saleappv1\eapp\test\product0.png')
+
+    time.sleep(1)
+
+    login = LoginPage(driver=driver)
+    login.open_page()
+    login.login(username='demodemo', password='Demo@123')
+
+    time.sleep(1)
+
+    assert driver.current_url == 'http://127.0.0.1:5000/'
+
+    e = driver.find_element(By.CSS_SELECTOR, '#collapsibleNavbar > ul > li:nth-child(5) > a')
+    assert (f'Chào demodemo' in e.text)
